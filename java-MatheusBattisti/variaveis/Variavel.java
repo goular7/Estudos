e91de52c106cf5bb6a97db92e311e82d32992cb4
goular7 <gustavogoulart1970@gmail.com>
2025-02-05 06:59:50 -0300

    variaveis

--- java-MatheusBattisti/variaveis/Variavel.java
+++ java-MatheusBattisti/variaveis/Variavel.java
@@ -11,6 +11,7 @@ public class Variavel {
         // nome => "Gustavo"
         System.out.println(nome);
 
+
         // 2 - Atribuição de variável com outra
         String teste = "Teste";
 
@@ -23,6 +24,53 @@ public class Variavel {
         // int numeroGrandeDois = numeroGrande;
 
         System.out.println(numeroGrande);
+
+
+        // 3 - Comentários
+
+        /* varias linhas
+
+        Comentario linha 1
+        Comentário linha 2
+        Comentário linha 3
+
+         */
+
+
+         /* Documentação
+          * 
+          * Função para somar números
+          * param1: Primeiro número
+          * param2: Segundo número
+          *
+          * Retorno: A soma
+          *
+          */
+
+
+        // 4 - String
+
+        String firstName = "Gustavo";
+        String lastName = "Goulart";
+
+        System.out.println(firstName + " " + lastName);
+
+        System.out.println("O nome dele é: " + firstName);
+
+        String fullName = firstName + " " + lastName;
+
+        // concatenação = operador + nas strings, para uni-las
+        System.out.println(fullName);
+
+
+        // 5 - Char
+        char letra = 'G';
+
+        System.out.println(letra);
+
+        char simbolo = '$';
+
+        System.out.println(simbolo);
     }
     
 }
