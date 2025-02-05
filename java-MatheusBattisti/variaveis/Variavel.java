3d3dbdbc2b16f28c3ec0d17d242a225879ca99a3
goular7 <gustavogoulart1970@gmail.com>
2025-02-05 06:43:50 -0300

    variaveis

---
+++ java-MatheusBattisti/variaveis/Variavel.java
@@ -0,0 +1,28 @@
+package variaveis;
+
+public class Variavel {
+
+    public static void main(String[] args) {
+        
+        // 1 - O que são variáveis
+        // tipo -> nome -> atribuir um valor;
+        String nome = "Gustavo";
+
+        // nome => "Gustavo"
+        System.out.println(nome);
+
+        // 2 - Atribuição de variável com outra
+        String teste = "Teste";
+
+        String testando = teste;
+
+        System.out.println(testando);
+
+        long numeroGrande = 999999999999L;
+
+        // int numeroGrandeDois = numeroGrande;
+
+        System.out.println(numeroGrande);
+    }
+    
+}
